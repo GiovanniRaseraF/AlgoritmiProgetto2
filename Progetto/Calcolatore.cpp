4780dfbbb016899c558a725f26dcc5db8679c9d4
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2020-08-07 13:32:15 +0200

    Implementazione del calcolo del tempo di AVL 	TODO: 		implementare Lambda per generalizzare l'operaione di calcolo del tempo

--- Progetto/Calcolatore.cpp
+++ Progetto/Calcolatore.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <chrono>
 #include <algorithm>
+#include <vector>
 #include <fstream>
 #include "Lambda.h"
 #include "Utils.h"
@@ -42,6 +43,12 @@ EFFETTO: nanosecondi
 long nanosec() { return duration_cast<nanosecondi>(steady_clock::now().time_since_epoch()).count(); }
 
 
+/*
+EFFETTO: Esegue gli inserimeti all'interno dell'albero e 
+ritorna il tempo che ci mette a fare l'operazione
+*/
+long inserimento(AVL::node* avl, int repetitionsCounter, double erroreMassimo);
+
 
 int main() {
 
@@ -65,21 +72,34 @@ int main() {
 	long samples = 100;
 	int numeroElementi = 0, prevNumeroElementi = 0;
 	int numeroIterazioni = 50;
+	int repetitionsCounter = 0;
 
 	//Calcolo dei tempi
 	for (int i = 0; i < samples; i++) {
 		//Calcolo il numero di elementi
 		double base = exp((log(maxSize) - log(minSize)) / (samples - 1));
-		double coefficiente = minSize;
+		repetitionsCounter = (int) ((long) initialRepetition * pow(base, i));
+		long coefficiente = minSize;
 		prevNumeroElementi = numeroElementi;
 		numeroElementi = round(coefficiente * pow(base, i));
 
-
-
 		//AVL Tree
+        long totalTimeAVL = 0f;
+		vector<long> tempMem{};
 		for(int iter = 0; iter < numeroIterazioni; iter++){
+			//Pulizia dell'albero
 			AVL::node::clean(avl);
+			//Conto il tempo che ci metto a fare gli iserimenti
+			long start = inserimento(avl, repetitionsCounter, erroreMassimo);
+			//Somma dei tempo
+			totalTimeAVL += start;
+			//Salvataggio del tempo per calcolo della varianza
+			tempMem.push_back(start);
 		}
+
+		//Calcolo del tempo ammortizzato
+		long tempoAmmortizzatoAVL = totalTimeAVL / repetitionsCounter;
+		long deviazioneAVL = calcolaDeviazione(tempMemAVL, repetitionsCounter);
 
 
 		//HEAP
